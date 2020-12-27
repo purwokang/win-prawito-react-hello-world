1eabe0b1dd017d7cae45b692f2c2fda11f022799
purwokang <purwo.com@gmail.com>
2020-12-27 21:32:20 +0700

    berhasil mengirim semua props time, title, dan desc

--- src/component/YouTubeComp/YouTubeComp.js
+++ src/component/YouTubeComp/YouTubeComp.js
@@ -8,14 +8,16 @@ const YouTubeComp = (props) => {
                 <img src="https://i.ytimg.com/vi/GBvnzRwoizo/hqdefault.jpg" alt="" />
                 <p className="time">{props.time}</p>
             </div>
-            <p className="title">title here</p>
-            <p className='desc' >desc here</p>
+            <p className="title">{props.title}</p>
+            <p className='desc'>{props.desc}</p>
         </div>
     )
 }
 
 YouTubeComp.defaultProps = {
-    time: '00.00'
+    time: '00.00',
+    title: 'Title here',
+    desc: 'xx ditonton. x hari yang lalu.'
 }
 
 export default YouTubeComp;
--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -7,10 +7,22 @@ class Home extends Component {
             <div>
                 <p>YouTube Component</p>
                 <hr />
-                <YouTubeComp time='7.12' />
-                <YouTubeComp time='8.02' />
-                <YouTubeComp time='5.04' />
-                <YouTubeComp time='4.12' />
+                <YouTubeComp
+                    time='7.12'
+                    title='Tutorial React JS - Bagian 1'
+                    desc='2x ditonton. 2 hari yang lalu.' />
+                <YouTubeComp
+                    time='8.02'
+                    title='Tutorial React JS - Bagian 2'
+                    desc='200x ditonton. 10 hari yang lalu.' />
+                <YouTubeComp
+                    time='5.04'
+                    title='Tutorial React JS - Bagian 3'
+                    desc='250x ditonton. 20 hari yang lalu.' />
+                <YouTubeComp
+                    time='4.12'
+                    title='Tutorial React JS - Bagian 4'
+                    desc='230x ditonton. 100 hari yang lalu.' />
                 <YouTubeComp />
             </div>
         )
