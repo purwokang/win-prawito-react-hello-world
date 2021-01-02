58784d97796b388a7cc60506b59bf305a91d2464
purwokang <purwo.com@gmail.com>
2021-01-02 22:43:01 +0700

    berhasil mendapatkan dan mengubah value dari form yang akan dikirim

--- src/container/BlogPost/BlogPost.js
+++ src/container/BlogPost/BlogPost.js
@@ -36,14 +36,20 @@ class BlogPost extends Component {
             })
     }
 
+    handleFormChange = (event) => {
+        // console.log('form change', event);
+        // console.log('form change', event.target);
+        let formBlogPostNew = { ...this.state.formBlogPost };
+        // console.log(event.target.name);
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        }, () => {
+            console.log('value obj formBlogPost: ', this.state.formBlogPost);
+        })
+    }
+
     componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        //     .then(response => response.json())
-        //     .then(json => {
-        //         this.setState({
-        //             post: json
-        //         })
-        //     })
         this.getPostAPI();
     }
 
@@ -53,9 +59,9 @@ class BlogPost extends Component {
                 <p className="section-title">Blog Post</p>
                 <div className="form-add-post">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" placeholder="add title" />
-                    <label htmlFor="body-content">Blog Content</label>
-                    <textarea name="body-content" id="body-content" cols="30" rows="10" placeholder="add blog content" ></textarea>
+                    <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange} />
+                    <label htmlFor="body">Blog Content</label>
+                    <textarea name="body" id="body" cols="30" rows="10" placeholder="add blog content" onChange={this.handleFormChange} ></textarea>
                     <button className="btn-submit">Simpan</button>
                 </div>
                 {
