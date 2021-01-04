00a63991fab1600785ff039f993d61c6c670e012
purwokang <purwo.com@gmail.com>
2021-01-04 23:21:03 +0700

    berhasil mengimplementasikan react router dasar

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from 'react';
 // import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-// import Product from '../Product/Product';
-// import BlogPost from '../BlogPost/BlogPost';
-import { BrowserRouter } from 'react-router-dom';
+import Product from '../Product/Product';
+import BlogPost from '../BlogPost/BlogPost';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './Home.css'
 import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
 
@@ -11,32 +11,18 @@ class Home extends Component {
         showComponent: true
     }
 
-    componentDidMount() {
-        // setTimeout(() => {
-        //     this.setState({
-        //         showComponent: false
-        //     })
-        // }, 15000)
-    }
-
     render() {
         return (
             <BrowserRouter>
                 <Fragment>
-                    {/* <div className="navigation">
+                    <div className="navigation">
                         <Link to="/" >Blog Post</Link>
                         <Link to="/product" >Product</Link>
-                    </div> */}
-                    <p>LifeCycle Component</p>
-                    <hr />
-                    {
-                        this.state.showComponent
-                            ?
-                            <LifeCycleComp />
-                            : null
-                    }
-                    {/* <Route path="/" exact component={BlogPost} />
-                    <Route path="/product" component={Product} /> */}
+                        <Link to="/lifecycle" >LifeCycleComp</Link>
+                    </div>
+                    <Route path="/" exact component={BlogPost} />
+                    <Route path="/product" component={Product} />
+                    <Route path="/lifecycle" component={LifeCycleComp} />
                 </Fragment>
             </BrowserRouter>
         )
