f43cea0dfee901ef5c0e80ca7bdb90e08409e138
purwokang <purwo.com@gmail.com>
2021-01-04 22:43:52 +0700

    menambahkan komponen LifeCycleComp agar sama dengan tutorial

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,21 +1,42 @@
 import React, { Component, Fragment } from 'react';
-import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-import Product from '../Product/Product';
-import BlogPost from '../BlogPost/BlogPost';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+// import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
+// import Product from '../Product/Product';
+// import BlogPost from '../BlogPost/BlogPost';
+import { BrowserRouter } from 'react-router-dom';
 import './Home.css'
+import LifeCycleComp from '../LifeCycleComp/LifeCycleComp';
 
 class Home extends Component {
+    state = {
+        showComponent: true
+    }
+
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.setState({
+        //         showComponent: false
+        //     })
+        // }, 15000)
+    }
+
     render() {
         return (
             <BrowserRouter>
                 <Fragment>
-                    <div className="navigation">
+                    {/* <div className="navigation">
                         <Link to="/" >Blog Post</Link>
                         <Link to="/product" >Product</Link>
-                    </div>
-                    <Route path="/" exact component={BlogPost} />
-                    <Route path="/product" component={Product} />
+                    </div> */}
+                    <p>LifeCycle Component</p>
+                    <hr />
+                    {
+                        this.state.showComponent
+                            ?
+                            <LifeCycleComp />
+                            : null
+                    }
+                    {/* <Route path="/" exact component={BlogPost} />
+                    <Route path="/product" component={Product} /> */}
                 </Fragment>
             </BrowserRouter>
         )
