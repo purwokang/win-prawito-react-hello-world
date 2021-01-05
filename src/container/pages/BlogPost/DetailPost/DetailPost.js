e5f22a518972726bb21a4ac88971cfed473fe941
purwokang <purwo.com@gmail.com>
2021-01-05 08:05:31 +0700

    berhasil menampilkan detail post dan memberikan sedikit style

--- src/container/pages/BlogPost/DetailPost/DetailPost.js
+++ src/container/pages/BlogPost/DetailPost/DetailPost.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
+import './DetailPost.css';
 
 class DetailPost extends Component {
+  state = {
+    post: {
+      title: '',
+      body: ''
+    }
+  }
   componentDidMount() {
-    console.log(this.props.match.params.postID);
+    // console.log(this.props);
+    // console.log(this.props.match.params.postID);
+
+    let id = this.props.match.params.postID;
+    Axios.get(`http://localhost:3004/posts/${id}`)
+      .then(res => {
+        console.log('result: ', res);
+        let post = res.data;
+        this.setState({
+          post: {
+            title: post.title,
+            body: post.body
+          }
+        })
+      })
+
   }
   render() {
     // console.log(this.props);
     return (
-      <p>Detail Post</p>
+      <div className="p-detail-post">
+        <p className="detail-title">{this.state.post.title}</p>
+        <p className="detail-body">{this.state.post.body}</p>
+      </div>
     )
   }
 }
