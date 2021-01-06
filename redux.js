856444bb6fe6dfa8f4ca1d409799b3eaf58fdd87
purwokang <purwo.com@gmail.com>
2021-01-06 15:05:54 +0700

    mulai bekerja dengan redux

---
+++ redux.js
@@ -0,0 +1,22 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+// ! 4 step dalam memahami redux
+
+// * Store
+// Sebuah wadah besar yang menyimpan value atau state secara global
+
+// * Reducer
+// Suatu fungsi yang digunakan untuk mengupdate store. Hanya reducer yang bisa mengupdate store.
+// Reducer biasa disebut juga sebagai agen, yang memiliki banyak task list
+
+
+
+// * Dispatching Action
+// Proses pemanggilan instruksi / task list yang disediakan reducer 
+
+
+// * Subscription
+// Proses pemanggilan store yang kita perlukan
+
+
